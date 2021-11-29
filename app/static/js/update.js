7c1f4c623f1c878bcdb0825a56acdee9faac04d6
pedroluisbernardos <kk391035@ens.uqam.ca>
2021-11-29 17:24:56 -0500

    update

--- app/static/js/update.js
+++ app/static/js/update.js
@@ -5,21 +5,28 @@
 //}
 
 document.getElementById("recherche").addEventListener("submit", function() {
-    var glissade = new Object();
-    var arrondissement = new Object();
-    glissade.nom = document.getElementById("nom").value;
-    arrondissement.nom_arr = document.getElementById("nom_arr").value
-    arrondissement.cle = document.getElementById("cle").value;
-    arrondissement.date_maj = document.getElementById("date_maj").value;
-    glissade.arrondissement = arrondissement;
-    glissade.ouvert = parseInt(document.getElementById("ouvert").value);
-    glissade.deblaye = parseInt(document.getElementById("deblaye").value);
-    glissade.condition = document.getElementById("condition").value;
+    var json_installation = new Object();
+    if (installation === 'glissade') {
+        var arrondissement = new Object();
+        json_installation.nom = document.getElementById("nom").value;
+        arrondissement.nom_arr = document.getElementById("nom_arr").value
+        arrondissement.cle = document.getElementById("cle").value;
+        arrondissement.date_maj = document.getElementById("date_maj").value;
+        json_installation.arrondissement = arrondissement;
+        json_installation.ouvert = parseInt(document.getElementById("ouvert").value);
+        json_installation.deblaye = parseInt(document.getElementById("deblaye").value);
+        json_installation.condition = document.getElementById("condition").value;
+    } else if (installation === 'patinoire') {
+
+    } else if (installation === 'piscine') {
+
+    }
+    
     event.preventDefault()
     fetch(url, {
         method:'PUT',
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(glissade)
+        body: JSON.stringify(json_installation)
     })
     .then(response => response.text())
     .then(response => JSON.parse(response))
